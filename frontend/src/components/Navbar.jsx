5fc5ef021b17b0a4924a31ba322c8118200368ff
emreeken183425 <emreeken1834@gmail.com>
2022-12-09 11:30:14 +0300

    css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,26 +8,26 @@ function Navbar() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-primary">
+      <nav className="navbar navbar-expand-lg bg-primary ">
         <div className="container-fluid">
           <h1 onClick={()=>navigate('/')} className="navbar-brand" >
             LİBRARY
           </h1>
-          <div className="collapse navbar-collapse" >
-            <ul className="navbar-nav me-auto mb-4 mb-lg-0">
+          <div className="collapse navbar-collapse  " >
+            <ul className="navbar-nav me-auto mb-3 mb-lg-0">
               <li className="nav-item">
-                <h1 onClick={()=>navigate('/')}  className="nav-link active"  >
+                <h1 onClick={()=>navigate('/')}  className="nav-link active text-white"  >
                   HOME
                 </h1>
               </li>
               <li className="nav-item">
-                <h1 className="nav-link active" onClick={()=>navigate('/bookk')} >
+                <h1 className="nav-link active text-white" onClick={()=>navigate('/bookk')} >
                   BOOOKS
                 </h1>
               </li>
               
               <li className="nav-item">
-                <h1 onClick={()=>navigate('/addbook')} className="nav-link active ">ADD BOOKS</h1>
+                <h1 onClick={()=>navigate('/addbook')} className="nav-link active text-white ">ADD BOOKS</h1>
               </li>
             </ul>
             
